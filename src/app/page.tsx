21cd0308d862ef2a3c7a9373f912d123a5fe9c17
VicFrolov <vicfrolov@gmail.com>
2023-09-10 20:24:57 -0700

    Adds send crypto widget.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useAccount } from 'wagmi';
 
 import { ConnectionButton } from '@/app/components/buttons/ConnectionButton/ConnectionButton';
 import { NetworkSelector } from '@/app/components/NetworkSelector/NetworkSelector';
+import { SendTransaction } from '@/app/components/SendTransaction/SendTransaction';
 import { WalletDetails } from '@/app/components/WalletDetails/WalletDetails';
 
 export default function Home() {
@@ -12,13 +13,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen p-24">
       <div className="flex flex-row justify-between w-full">
-        <div className="flex flex-col">
-          <WalletDetails
-            address={address}
-            isConnected={isConnected}
-            isConnecting={isConnecting}
-          />
-          <NetworkSelector />
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <WalletDetails
+              address={address}
+              isConnected={isConnected}
+              isConnecting={isConnecting}
+            />
+            <NetworkSelector />
+          </div>
+          <div className="ml-16">
+            <SendTransaction />
+          </div>
         </div>
         <div className="flex">
           <ConnectionButton />
